b84cb04b0a2c8584fa73ddb267e5b0126424f75b
Paolo Piparo <50234017+paolopiparo@users.noreply.github.com>
2024-06-16 01:25:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,24 +31,12 @@ with col4:
 future_list = df.columns.tolist()
 genres_selection = st.multiselect('Exclude Future', future_list)
 
-# Apply conditional formatting
-def gradient_bgcolor(val, vmin, vmax):
-    normalized = (val - vmin) / (vmax - vmin)
-    r = int(255 * (1 - normalized))
-    g = int(255 * normalized)
-    b = 0
-    return f'background-color: rgb({r},{g},{b})'
-
-
-
-# Display the DataFrame with conditional background coloring
-st.write("Styled DataFrame:")
-for col in df.columns:
-    # Find min and max for each column
-    vmin, vmax = df[col].min(), df[col].max()
-    # Apply conditional formatting using pandas style.applymap
-    st.write(df[col].apply(lambda x: f'background-color: {gradient_bgcolor(x, vmin, vmax)}'), unsafe_allow_html=True)
-
+if genres_selection:
+  
+  filtered_df = df.drop(columns=genres_selection)
+  st.write(filtered_df.style.background_gradient(cmpa="Blues"))
+else:
+  st.write(df)
 
 
 
