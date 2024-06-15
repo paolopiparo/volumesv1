f418284e7db1209e0edca895fb32a9a87b6621f8
Paolo Piparo <50234017+paolopiparo@users.noreply.github.com>
2024-06-16 01:04:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,10 @@ st.title('Volumes Tables')
 
 st.subheader('Historical Volumes')
 
-col1, col2, col3 = st.columns(3)
+df = pd.read_csv('data/cum_volume_random.csv')
+df.rename(columns={df.columns[0]:"Interval"]}, inplace=True)
+
+col1, col2, col3, col4 = st.columns(4)
 
 with col1:
   options = ["Cumulative Volumes", "Standard Deviation", "% vs Average", "DV01 vs average"]
@@ -20,16 +23,20 @@ with col2:
 with col3:
   radio_options = ["5 Minutes", "10 Minutes", "15 Minutes"]
   selected_radio = st.radio("Select interval:", radio_options)
+with col4: 
+  slider_values = [5, 10, 15, 20, 30, 40, 50]
+  slider = st.select_slider("Select Average period:", options=slider_values, value=slider_values[0])
 
-slider_values = [5, 10, 15, 20, 30, 40, 50]
-slider = st.select_slider("Select Average period:", options=slider_values, value=slider_values[0])
+future_list = df.columns.tolist()
+genres_selection = st.multiselect('Exclude Future', future_list)
 
-# Load data
-#path = "V:/piparp/BACKTESTS/Volume_NC_Project/Backup/Testing/all/Tables_Historical/"
 
-#csv_url = "https://github.com/paolopiparo/volumesv1/blob/master/data/cum_volume_random.csv"
-df = pd.read_csv('data/cum_volume_random.csv')
-st.write(df)
+if genres_selection:
+  
+  filtered_df = df.drop(columns=genres_selection)
+  st.write(filtered_df)
+else:
+  st.write(df)
 
 #df.year = df.year.astype('int')
 
